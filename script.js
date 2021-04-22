5f546d5cb90678b21bca1cd4d4e48e8d0cbaf626
Inès C <ines.parisdescartes@gmail.com>
2021-04-22 12:08:25 +0200

    modal js : reservia

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-let btnSignIn = document.getElementById('sign-in');
-btnSignIn.addEventListener('click', function() {
-    let modalSignUp = document.getElementById('sign-up');
-    modalSignUp.style.display = 'block';
+/** open modal sign-up */
+let btnSignIn = document.getElementById("sign-in");
+btnSignIn.addEventListener("click", function () {
+    let modalSignUp = document.getElementById("sign-up");
+    modalSignUp.style.display = "block";
 });
 
-let btnModalClose = document.getElementById('close-sign')
-btnModalClose.addEventListener('click', function() {
-    let modalclose = document.getElementById('sign-up');
-    modalclose.style.display = 'none';
+/** close modal sign-up */
+let btnModalClose = document.getElementById("close-sign");
+btnModalClose.addEventListener("click", function () {
+    let modalclose = document.getElementById("sign-up");
+    modalclose.style.display = "none";
 });
 
-let btnConnect = document.querySelector('#connect');
-
-btnConnect.addEventListener('click', function(){
-    let modalclose = document.getElementById('sign-up');
-    modalclose.style.display = 'none';
-    let modalConnect = document.getElementsByClassName('connect');
-    modalConnect.style.display = 'block';
+/** close modal sign-up and open modal connect */
+let btnConnect = document.getElementById("link-connect");
+btnConnect.addEventListener("click", function (event) {
+    event.preventDefault;
+    let modalclose = document.getElementById("sign-up");
+    modalclose.style.display = "none";
+    let modalConnect = document.getElementById("connect");
+    modalConnect.style.display = "block";
 });
 
-
-
-
-
-
-
-
+/** close modal connect */
+let btnModalCloseConnect = document.getElementById("close-connect");
+btnModalCloseConnect.addEventListener("click", function () {
+    let modalclose = document.getElementById("connect");
+    modalclose.style.display = "none";
+});
